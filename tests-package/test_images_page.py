6470285dafa3a2c491451667baffc5cdd7382244
REGION\va.sinkelev <v.sinkelev@yandex.ru>
2021-07-01 14:42:42 +0300

    Fix test_images

--- tests-package/test_images_page.py
+++ tests-package/test_images_page.py
@@ -16,22 +16,24 @@ class TestSearch(TestCaseUI):
 
     def test_images_search_in_yandex(self):
         log('Проверяем наличие ссылки на картинки')
-        MainPage(self.driver).should_be_images_link()
+        main = MainPage(self.driver)
+        main.should_be_images_link()
 
         log('Переход на сервис картинок')
-        MainPage(self.driver).click_on_image_link()
-        ImagesPage(self.driver).switch_to_last_tab()
+        main.click_on_image_link()
+        images = ImagesPage(self.driver)
+        images.switch_to_last_tab()
 
         log('Проверка url')
-        ImagesPage(self.driver).should_be_images_url('https://yandex.ru/images/')
+        images.should_be_images_url('https://yandex.ru/images/')
 
         log('Открытие первой категории картинок и сравнение текста')
-        ImagesPage(self.driver).open_and_test_first_category()
+        images.open_and_test_first_category()
 
         log('Проверка укрупненного просмотра картинок')
-        ImagesPage(self.driver).should_be_block_images_search()
-        ImagesPage(self.driver).open_and_check_first_image()
-        ImagesPage(self.driver).go_on_next_and_prev_images()
+        images.should_be_block_images_search()
+        images.open_and_check_first_image()
+        images.go_on_next_and_prev_images()
 
 
 if __name__ == '__main__':
